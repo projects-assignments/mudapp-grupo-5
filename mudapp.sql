a69c6ccc151a468196b5e831c40596c0da5f5c9a
Crisitan Salas <148869135+ctupac999@users.noreply.github.com>
2024-01-15 15:13:34 +0100

    mudapp15/01 ok

--- mudapp.sql
+++ mudapp.sql
@@ -31,6 +31,22 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Vehicle`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Vehicle` (
+  `vehicle_id` INT NOT NULL,
+  `vehicle_brand` VARCHAR(45) NULL,
+  `vehicle_model` VARCHAR(45) NULL,
+  `vehicle_licenseplate` VARCHAR(45) NULL,
+  `vehicle_height` VARCHAR(45) NULL,
+  `vehicle_length` VARCHAR(45) NULL,
+  `vehicle_width` VARCHAR(45) NULL,
+  `vehicle_weight` VARCHAR(45) NULL,
+  PRIMARY KEY (`vehicle_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Carrier`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Carrier` (
@@ -44,30 +60,25 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Carrier` (
   `carrier_address` VARCHAR(45) NULL,
   `carrier_maxkm` VARCHAR(45) NULL,
   `carrier_password` VARCHAR(10) NULL,
-  PRIMARY KEY (`carrier_id`))
+  PRIMARY KEY (`carrier_id`),
+  INDEX `fk_vehicle_id_idx` (`vehicle_id` ASC) VISIBLE,
+  CONSTRAINT `fk_vehicle_id`
+    FOREIGN KEY (`vehicle_id`)
+    REFERENCES `mydb`.`Vehicle` (`vehicle_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Vehicle`
+-- Table `mydb`.`Payment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Vehicle` (
-  `vehicle_id` INT NOT NULL,
-  `vehicle_brand` VARCHAR(45) NULL,
-  `vehicle_model` VARCHAR(45) NULL,
-  `vehicle_licenseplate` VARCHAR(45) NULL,
-  `vehicle_height` VARCHAR(45) NULL,
-  `vehicle_length` VARCHAR(45) NULL,
-  `vehicle_width` VARCHAR(45) NULL,
-  `vehicle_weight` VARCHAR(45) NULL,
-  `Carrier_carrier_id` INT NOT NULL,
-  PRIMARY KEY (`vehicle_id`, `Carrier_carrier_id`),
-  INDEX `fk_Vehicle_Carrier1_idx` (`Carrier_carrier_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Vehicle_Carrier1`
-    FOREIGN KEY (`Carrier_carrier_id`)
-    REFERENCES `mydb`.`Carrier` (`carrier_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`Payment` (
+  `payment_id` INT NOT NULL,
+  `payment_method` VARCHAR(45) NULL,
+  `payment_amount` VARCHAR(45) NULL,
+  `payment_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`payment_id`))
 ENGINE = InnoDB;
 
 
@@ -90,53 +101,37 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`Service` (
   `service_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `carrier_id` INT NOT NULL,
+  `carrier_id` INT NULL,
   `service_date` VARCHAR(45) NULL,
   `service_time` VARCHAR(45) NULL,
   `service_departureaddress` VARCHAR(45) NULL,
   `service_destinationaddress` VARCHAR(45) NULL,
   `service_remarks` VARCHAR(45) NULL,
-  `Rating Carrier_rating_id` INT NOT NULL,
-  `Rating Carrier_Carrier_carrier_id` INT NOT NULL,
-  `User_user_id` INT NOT NULL,
-  `User_user_firstname` VARCHAR(20) NOT NULL,
-  `Carrier_carrier_id` INT NOT NULL,
+  `rating_id` INT NOT NULL,
+  `payment_id` INT NULL,
   PRIMARY KEY (`service_id`),
-  INDEX `fk_Transport Service_Rating Carrier1_idx` (`user_id` ASC, `service_id` ASC) VISIBLE,
-  INDEX `fk_Transport Service_User1_idx` (`User_user_id` ASC, `User_user_firstname` ASC) VISIBLE,
-  INDEX `fk_Transport Service_Carrier1_idx` (`Carrier_carrier_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Transport Service_Rating Carrier1`
-    FOREIGN KEY (`user_id` , `service_id`)
-    REFERENCES `mydb`.`Rating` (`rating_id` , `service_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Transport Service_User1`
-    FOREIGN KEY (`User_user_id` , `User_user_firstname`)
-    REFERENCES `mydb`.`User` (`user_id` , `user_firstname`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Transport Service_Carrier1`
-    FOREIGN KEY (`Carrier_carrier_id`)
+  INDEX `fk_payment_id_idx` (`payment_id` ASC) VISIBLE,
+  INDEX `fk_user_id_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_rating_id_idx` (`rating_id` ASC) VISIBLE,
+  INDEX `fk_carrier_id_idx` (`carrier_id` ASC) VISIBLE,
+  CONSTRAINT `fk_carrier_id`
+    FOREIGN KEY (`carrier_id`)
     REFERENCES `mydb`.`Carrier` (`carrier_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Payment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Payment` (
-  `payment_id` INT NOT NULL,
-  `payment_method` VARCHAR(45) NULL,
-  `service_id` INT NULL,
-  `payment_amount` VARCHAR(45) NULL,
-  `payment_status` VARCHAR(45) NULL,
-  PRIMARY KEY (`payment_id`),
-  INDEX `service_id_idx` (`service_id` ASC) VISIBLE,
-  CONSTRAINT `service_id`
-    FOREIGN KEY (`service_id`)
-    REFERENCES `mydb`.`Service` (`service_id`)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mydb`.`User` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_rating_id`
+    FOREIGN KEY (`rating_id`)
+    REFERENCES `mydb`.`Rating` (`rating_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_payment_id`
+    FOREIGN KEY (`payment_id`)
+    REFERENCES `mydb`.`Payment` (`payment_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
